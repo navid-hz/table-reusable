324e756e21d23a9529de259da90ea01255d2581c
Navid Hosseini <hosseini.zari@gmail.com>
2024-05-28 12:45:36 +0200

    style added

--- src/components/atoms/TdCell.jsx
+++ src/components/atoms/TdCell.jsx
@@ -8,4 +8,9 @@ const TdCell = ({ children }) => {
 export default TdCell;
 
 const Td = styled.td`
+    border: 1px solid #000;
+    padding: 8px;
+    text-align: center;
+    width: fit-content;
+    font-family: 'Arial', sans-serif;
 `;
--- src/components/atoms/ThCell.jsx
+++ src/components/atoms/ThCell.jsx
@@ -9,4 +9,10 @@ const ThCell = ({ children }) => {
 export default ThCell;
 
 const Th = styled.th`
+    border: 1px solid #000;
+    padding: 8px;
+    text-align: center;
+    width: fit-content;
+    background-color: #f2f2f2;
+    font-family: 'Arial', sans-serif;
 `;	
